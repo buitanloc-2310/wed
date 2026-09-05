7f44459e00a8f3a4a14e94286a5f9fda8105ba1f
buitanloc-2310 <buitanloccon@gmail.com>
2026-09-05 08:03:38 +0700

    Add files via upload

---
+++ migrations/0005_official_content_menu.sql
@@ -0,0 +1,3 @@
+INSERT INTO menu_items(lang,location,parent_id,label,url,sort_order,visible,new_tab) VALUES
+('vi','header',2,'Nội dung chính thức','/noi-dung',6,1,0),
+('en','header',102,'Official information','/noi-dung',6,1,0);
